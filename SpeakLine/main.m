9b3b163bc959999e9d4733a4ba037fd62015f1e9
MacRae Linton <macrae.linton@gmail.com>
2009-03-01 22:41:51 -0500

    Created the SpeakLine project.

---
+++ SpeakLine/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SpeakLine
+//
+//  Created by MacRae Linton on 3/1/09.
+//  Copyright Duke University 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
