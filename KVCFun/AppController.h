f9697b435a63c6bf73305ceee82c659dd1d3c44f
MacRae Linton <macrae.linton@gmail.com>
2009-03-02 16:01:47 -0500

    recreated README, and added the KVCFun project this is a merge.
    Conflicts:
    
    	README

---
+++ KVCFun/AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  KVCFun
+//
+//  Created by MacRae Linton on 3/2/09.
+//  Copyright 2009 Duke University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
