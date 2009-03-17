df33fc88fb4864772500aa7b00cc8b74cbe76307
MacRae Linton <macrae.linton@gmail.com>
2009-03-17 18:12:37 -0400

    Added the Carlot Project, with datamodel intact

---
+++ CarLot/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  CarLot
+//
+//  Created by MacRae Linton on 3/17/09.
+//  Copyright Duke University 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ CarLot/MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  CarLot
+//
+//  Created by MacRae Linton on 3/17/09.
+//  Copyright Duke University 2009 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
